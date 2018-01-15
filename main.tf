f23c62ed0c97b8c9fad29813a4acfdd1066a35f6
David F. Severski <davidski@deadheaven.com>
2018-01-15 14:37:37 -0800

    Templatize additional variables

--- main.tf
+++ main.tf
@@ -2,39 +2,55 @@ variable "origin_domain_name" {}
 variable "origin_id" {}
 variable "alias" {}
 variable "acm_certificate_arn" {}
-variable "origin_path" {
-  default     = ""
-}
-variable "origin_http_port" {
-  default     = 80
-}
-variable "origin_https_port" {
-  default     = 443
-}
-variable "distribution_enabled" {
-  default     = true
-}
-variable "comment" {
-  default     = ""
-}
-variable "default_root_object" {
-  default     = "index.html"
-}
-variable "compression" {
-  default     = false
+variable "project" {}
+
+variable "price_class" {
+  default = "PriceClass_100"
 }
 
+variable "minimum_protocol_version" {
+  default = "TLSv1.1_2016"
+}
+
+variable "origin_path" {
+  default = ""
+}
+
+variable "origin_http_port" {
+  default = 80
+}
+
+variable "origin_https_port" {
+  default = 443
+}
+
+variable "distribution_enabled" {
+  default = true
+}
+
+variable "comment" {
+  default = ""
+}
+
+variable "default_root_object" {
+  default = "index.html"
+}
+
+variable "compression" {
+  default = false
+}
 
 resource "aws_cloudfront_distribution" "ssl_distribution" {
   origin {
     domain_name = "${var.origin_domain_name}"
     origin_id   = "${var.origin_id}"
     origin_path = "${var.origin_path}"
+
     custom_origin_config {
-      http_port = "${var.origin_http_port}"
-      https_port = "${var.origin_https_port}"
-      origin_protocol_policy = "https-only" # Only talk to the origin over HTTPS
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      http_port              = "${var.origin_http_port}"
+      https_port             = "${var.origin_https_port}"
+      origin_protocol_policy = "https-only"                    # Only talk to the origin over HTTPS
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
 
@@ -42,8 +58,8 @@ resource "aws_cloudfront_distribution" "ssl_distribution" {
   comment             = "${var.comment}"
   default_root_object = "${var.default_root_object}"
 
-  aliases = ["${var.alias}"]
-  price_class = "PriceClass_200"
+  aliases     = ["${var.alias}"]
+  price_class = "${var.price_class}"
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -67,13 +83,18 @@ resource "aws_cloudfront_distribution" "ssl_distribution" {
 
   restrictions {
     geo_restriction {
-        restriction_type = "none"
+      restriction_type = "none"
     }
   }
 
   viewer_certificate {
-    acm_certificate_arn = "${var.acm_certificate_arn}"
-    ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1"
+    acm_certificate_arn      = "${var.acm_certificate_arn}"
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "${var.minimum_protocol_version}"
+  }
+
+  tags {
+    managed_by = "Terraform"
+    project    = "${var.project}"
   }
 }
