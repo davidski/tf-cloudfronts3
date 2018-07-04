3e65a70fdaf431738e5c7bbac2494f276b27ee8c
David F. Severski <davidski@deadheaven.com>
2018-07-04 14:15:33 -0700

    Export hosted_zone_id as output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "domain_name" {
   value = "${aws_cloudfront_distribution.ssl_distribution.domain_name}"
 }
 
+output "hosted_zone_id" {
+  value = "${aws_cloudfront_distribution.ssl_distribution.hosted_zone_id}"
+}
+
 output "bucket_server_arn" {
   value = "${aws_s3_bucket.cloudfront_bucket.arn}"
 }
